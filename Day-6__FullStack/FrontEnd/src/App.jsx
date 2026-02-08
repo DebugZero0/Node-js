dd82623eae5725078b48dda3451699c058c05afb
DebugZero0 <ankannew2024@gmail.com>
2026-02-09 01:21:30 +0530

    Fixed api bug

--- Day-6__FullStack/FrontEnd/src/App.jsx
+++ Day-6__FullStack/FrontEnd/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }, [])
 
   const fetchNotes = () => {
-    axios.get('https://node-js-lxpr.onrender.com//api/notes')
+    axios.get('https://node-js-lxpr.onrender.com/api/notes')
       .then((res) => setPost(res.data))
       .catch((err) => console.log(err))
   }
@@ -22,7 +22,7 @@ function App() {
     e.preventDefault()
 
     // POST request to create a new note
-    axios.post('https://node-js-lxpr.onrender.com//api/notes', {
+    axios.post('https://node-js-lxpr.onrender.com/api/notes', {
       title: title,
       description: description
     })
@@ -38,7 +38,7 @@ function App() {
   }
   const deleteNote = (id) => {
     // DELETE request to delete a note
-    axios.delete(`https://node-js-lxpr.onrender.com//api/notes/${id}`)
+    axios.delete(`https://node-js-lxpr.onrender.com/api/notes/${id}`)
       .then((res) => {
         console.log(res.data)
         // Refresh the notes list
@@ -61,7 +61,7 @@ function App() {
       return;
     }
     // PUT request to update a note
-    axios.patch(`https://node-js-lxpr.onrender.com//api/notes/${id}`, {
+    axios.patch(`https://node-js-lxpr.onrender.com/api/notes/${id}`, {
       title: newTitle,
       description: newDescription
     })
